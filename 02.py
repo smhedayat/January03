113462e02a71b753c21685989ea2415fd0b45949
smhedayat <researchwork15@gmail.com>
2024-01-12 19:51:15 +0530

    First commit in 02

---
+++ 02.py
@@ -0,0 +1,14 @@
+a = 3
+print("The variable is", a)
+b = 4
+c = a+b
+print('The sum is', c)
+hello = "Hello there!"
+print(hello)
+d = a+b+b
+x = 123456
+
+print("Hey there!")
+
+
+print('I like learning Github a lot')
